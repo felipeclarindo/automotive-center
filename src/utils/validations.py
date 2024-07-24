0be7506ab89a9d8a8060553bb752fe0122408425
felipeclarindo <fglpc2@gmail.com>
2024-07-24 12:31:15 -0300

    Adicionando __init__, gitignore e realizando testes

--- src/utils/validations.py
+++ src/utils/validations.py
@@ -12,17 +12,19 @@ def validateStringInput(input:str) -> bool:
         return validation
     except Exception as e:
         print(e)
+    return False
 
 def validateExit(confirm:str) -> bool:
     try:
         validation = False
-        confirm = confirm.strip()
-        if len(confirm) > 0:
-            validation = True if confirm.isalpha() and confirm.islower() == "sim" else False
+        confirm = confirm.strip().replace(" ", "")
+        if len(confirm) == 0:
+            raise ValueError("O valor não pode ser vazio!")
+        else:
+            validation = True if confirm.isalpha() and confirm.lower() in ["s", "n", "sim", "nao", "não"] else False
             if not validation:
                 raise ValueError("Valor invalido!")
-        else:
-            raise ValueError("O valor não pode ser vazio!")
         return validation
     except Exception as e:
         print(e)
+    return False
